b68641eb417ac10dfffda353c7f86dd4525d1b03
nnmhd <natchanone@gmail.com>
2024-06-29 11:06:01 -0400

    Add Reset Button
    In this commit, the reset button has been accessed and functioned, the puzzle pieces are back to the default position, Also the board background changes to the default image.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-console.log("JS is connected")
+console.log("JS is connected");
+// --  Bug fixed by Natchanon Nate Mahaittidon w/ @rational --//
 
 // variables
 const theButtons = document.querySelectorAll("#buttonHolder img"),
-puzzleBoard = document.querySelector(".puzzle-board"),
-puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
-dropZones = document.querySelectorAll(".drop-zone");
+  puzzleBoard = document.querySelector(".puzzle-board"),
+  puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
+  // @ Add Puzzle Container to access the puzzle pieces (class = puzzle-pieces)
+  puzzleContainer = document.querySelector(".puzzle-pieces"),
+  // @ Accessing to the reset button
+  resetButton = document.querySelector("#resetBut"),
+  dropZones = document.querySelectorAll(".drop-zone");
 let draggedPiece;
 
 console.log(theButtons);
@@ -13,34 +18,60 @@ console.log(puzzleBoard);
 // functions
 
 function changeBGImage() {
-    console.log(this.id)
-    // background-image: url('../images/backGround0.jpg');
-    puzzleBoard.style.backgroundImage = `url('../images/backGround${this.id}.jpg'`;
-    // Method 2
-    console.log(event.currentTarget.id)
-    puzzleBoard.style.backgroundImage = `url('../images/backGround${event.currentTarget.id}.jpg'`;
+  console.log(this.id);
+  // background-image: url('../images/backGround0.jpg');
+  puzzleBoard.style.backgroundImage = `url('../images/backGround${this.id}.jpg'`;
+  // Method 2
+  console.log(event.currentTarget.id);
+  puzzleBoard.style.backgroundImage = `url('../images/backGround${event.currentTarget.id}.jpg'`;
 }
 
 function handleStartDrag() {
-    console.log(`started dragging ${this}`)
-    draggedPiece = this;
+  console.log(`started dragging ${this}`);
+  draggedPiece = this;
 }
 
 function handleOver(e) {
-    e.preventDefault();
-    console.log("Dragged Over");
+  e.preventDefault();
+  console.log("Dragged Over");
 }
 
 function handleDrop() {
-    this.appendChild(draggedPiece);
+  this.appendChild(draggedPiece);
+}
+
+// @ Add function to reset game, all of positions have reset to default
+function resetGame() {
+  dropZones.forEach((zone) => {
+    while (zone.firstChild) {
+      zone.removeChild(zone.firstChild);
+    }
+  });
+
+  // @ To reset all the puzzle pieces and board to the first image as start
+  puzzlePieces[0].src = `images/topLeft0.jpg`;
+  puzzlePieces[1].src = `images/topRight0.jpg`;
+  puzzlePieces[2].src = `images/bottomLeft0.jpg`;
+  puzzlePieces[3].src = `images/bottomRight0.jpg`;
+  puzzleBoard.style.backgroundImage = `url('../images/backGround0.jpg'`;
+
+  puzzlePieces.forEach((piece) => {
+    puzzleContainer.appendChild(piece);
+  });
 }
 
 // eventListeners
 
-theButtons.forEach(button => button.addEventListener("click", changeBGImage));
+theButtons.forEach((button) => button.addEventListener("click", changeBGImage));
 
-puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
+puzzlePieces.forEach((piece) =>
+  piece.addEventListener("dragstart", handleStartDrag)
+);
 
-dropZones.forEach(zone => zone.addEventListener("dragover", handleOver));
+dropZones.forEach((zone) => zone.addEventListener("dragover", handleOver));
 
-dropZones.forEach(zone => zone.addEventListener("drop", handleDrop));
+dropZones.forEach((zone) => zone.addEventListener("drop", handleDrop));
+
+// @ Add the event listener for reset button, click to call resetGame()
+
+resetButton.addEventListener("click", resetGame);
